c0b709d8ca4f30895265641f6981cf32116dc0ed
ron_the_rocker <96737425+superbiscota@users.noreply.github.com>
2022-06-14 22:49:13 +0900

    Add files via upload

--- t.py
+++ t.py
@@ -1,12 +1,9 @@
-#from concurrent.futures import ThreadPoolExecutor
-import threading
+import joblib
 import hashlib
 import time
 
 name          =  "ron"
 target_id     =  "5befaa85b5176954561d90ab40df4718"
-MAX_WORKER    = 4
-
 
 MAX_IP_ADRESS_RANGE  =  4278190079 + 1 # 4 thred で割り切れるようにするための + 1
 SKIP_IP_ADRESS_ZONE  =  16777216
@@ -32,37 +29,19 @@ def ip_generator(index):
 	A =  (index >> 24) 
 	return "{a}.{b}.{c}.{d}".format (a = A ,b = B ,c = C ,d = D)
 
+start_time = time.time()
 
-def sweeper(label,from_num , to_num) :
-	start_time = time.time()
-	for itr in range(from_num , to_num) :
-		deci_percent = int (  ( to_num - from_num ) / 1000 ) 
-		ip_adress  = ip_generator(itr + SKIP_IP_ADRESS_ZONE )
-		if (itr % deci_percent == 0 and itr > 0) : # 0.1% = deci %		
-			now_time = time.time()
-			past_time = now_time - start_time # 秒
+def sweeper(itr) :
+	#for itr in range(from_num , to_num) :
+	deci_percent = int (  ( MAX_IP_ADRESS_RANGE ) / 1000 ) 
+	ip_adress  = ip_generator(itr + SKIP_IP_ADRESS_ZONE )
+	if (itr % deci_percent == 0 and itr > 0) : # 0.1% = deci %		
+		now_time = time.time()
+		past_time = now_time - start_time # 秒
 		 # revolution per sec
-			print ( "process " + label +  "  " + str (int(itr/( to_num - from_num )  * 10000)/100) + "%   " + str (int (itr / past_time) ) + " r/sec "  + " 《" + ip_adress + "》 ")
-		ret = check(target_id,ip_adress)
-		if (ret == 1) : 
-			break
-
-
-
+		print ( "process " +  str (int(itr/( MAX_IP_ADRESS_RANGE ) * 10000)/100) + "%   " + str (int (itr / past_time) ) + " r/sec "  + " 《" + ip_adress + "》 ")
+	ret = check(target_id,ip_adress)
 
 #tpe = ThreadPoolExecutor(max_workers=MAX_WORKER)
 
-thread1 = threading.Thread(target=sweeper("A",                          1       , int  (MAX_IP_ADRESS_RANGE/4 * 1) ))
-thread2 = threading.Thread(target=sweeper("B", int (MAX_IP_ADRESS_RANGE/4*1) +1 , int  (MAX_IP_ADRESS_RANGE/4 * 2) ))
-thread3 = threading.Thread(target=sweeper("C", int (MAX_IP_ADRESS_RANGE/4*2) +1 , int  (MAX_IP_ADRESS_RANGE/4 * 3) ))
-thread4 = threading.Thread(target=sweeper("D", int (MAX_IP_ADRESS_RANGE/4*3) +1 , int  (MAX_IP_ADRESS_RANGE/4 * 4) ))
-
-thread1.start()
-thread2.start()
-thread3.start()
-thread4.start()
-
-thread1.join()
-thread2.join()
-thread3.join()
-thread4.join()
+joblib.Parallel(n_jobs=-1)(joblib.delayed(sweeper)(i) for i in range( 1       , int  (MAX_IP_ADRESS_RANGE )))
